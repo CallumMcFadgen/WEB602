34a90d83ff10ecf1b18f57f93d2e74be23f0dad8
CallumMcFadgen <callum.mcfadgen@gmail.com>
2019-08-10 06:07:05 +1200

    JS homework

---
+++ JavaScriptHomework/Homework1.js
@@ -0,0 +1,12 @@
+
+//Create a function that takes two numbers as arguments and return their sum
+
+const numOne = 5;
+const numTwo = 4;
+
+function SimpleAddition(NumOne, NumTwo){
+let result = (numOne + numTwo);
+return console.log(result);
+};
+
+SimpleAddition(3, 6);
