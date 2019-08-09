6a3b50a34a7c5943113e937824e36c4985fee3ac
CallumMcFadgen <callum.mcfadgen@gmail.com>
2019-08-10 06:41:10 +1200

    Javascript Homework

--- JavaScriptHomework/Homework1.js
+++ JavaScriptHomework/Homework1.js
@@ -1,12 +1,31 @@
+// 1 - Return the sum of two number
+// Create a function that takes two numbers as arguments and returns their sum.
 
-//Create a function that takes two numbers as arguments and return their sum
 
-const numOne = 5;
-const numTwo = 4;
+const paramOne = 5;
+const paramTwo = 4;
 
-function SimpleAddition(NumOne, NumTwo){
-let result = (numOne + numTwo);
-return console.log(result);
+function AddNumbers(paramOne, paramTwo){
+  let result = (paramOne + paramTwo);
+  return console.log(result);
 };
 
-SimpleAddition(3, 6);
+AddNumbers();
+
+
+// 2 - Return the Remainder from Two Numbers
+// There is a single operator in JavaScript capable of providing the remainder of a division operation. 
+// Two numbers are passed as parameters. 
+// The first provider divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+
+const startingParam = 5;
+const divisonParam = 2;
+
+function FindRemainder(startingParam, divisonParam){
+  let remainder = (startingParam % divisonParam);
+  return console.log(remainder);
+};
+
+FindRemainder()
+
