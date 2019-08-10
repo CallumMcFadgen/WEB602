26c07639e32fa53fba8dd96914525a42b5ab46f0
CallumMcFadgen <callum.mcfadgen@gmail.com>
2019-08-11 11:42:09 +1200

    homework revisited

--- JavaScriptHomework/Homework1.js
+++ JavaScriptHomework/Homework1.js
@@ -1,33 +1,54 @@
-// 1 - Return the sum of two number
+// 1 - Return the sum of two numbers
 // Create a function that takes two numbers as arguments and returns their sum.
 
+// 1st option
+console.log(1 + 2);
 
+//2nd option
 let paramOne = null;
 let paramTwo = null;
-
-function AddNumbers(paramOne, paramTwo)
+function BasicAdditionV1(paramOne, paramTwo)
 {
-  const result = (paramOne + paramTwo);
+  let result = (paramOne + paramTwo);
   return console.log(result);
 };
 
-AddNumbers(4, 5);
+BasicAdditionV1(1, 2);
+
+//3rd option (removed variable decleration as not needed)
+function BasicAdditionV2( paramOne, paramTwo){
+  let result = (paramOne + paramTwo);
+  return console.log(result);
+};
+
+BasicAdditionV2(1, 2);
 
 
-// 2 - Return the Remainder from Two Numbers
-// There is a single operator in JavaScript capable of providing the remainder of a division operation. 
-// Two numbers are passed as parameters. 
-// The first provider divided by the second parameter will have a remainder, possibly zero. Return that value.
+// // 2 - Return the Remainder from Two Numbers
+// // There is a single operator in JavaScript capable of providing the remainder of a division operation. 
+// // Two numbers are passed as parameters. 
+// // The first provider divided by the second parameter will have a remainder, possibly zero. Return that value.
 
+// 1st option
+console.log(5 % 2);
 
+// 2nd option
 let startingParam = null;
 let divisonParam = null;
-
-function FindRemainder(startingParam, divisonParam)
+function FindRemainderV1(startingParam, divisonParam)
 {
-  const remainder = (startingParam % divisonParam);
+  let remainder = (startingParam % divisonParam);
   return console.log(remainder);
 };
 
-FindRemainder(5,2)
+FindRemainderV1(5, 2);
 
+
+// 3rd option
+function FindRemainderV2(paramOne, paramTwo)
+{
+  let remainder = (paramOne % paramTwo);
+  return console.log(remainder);
+};
+
+FindRemainderV2(5, 2);
---
+++ practice.js
@@ -0,0 +1,6 @@
+
+//  const name = "Callum";
+//  const age = 30;
+
+//  console.log('my name is '+ name +' my age is ' + age);
+
